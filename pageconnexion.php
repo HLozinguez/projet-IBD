79dadcc34d01cc56da32aafedf31bc9d16b59109
juliettemnr <59599940+juliettemnr@users.noreply.github.com>
2021-03-13 17:44:08 +0100

    Add files via upload

---
+++ pageconnexion.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+		<form action="verif.php" method="post">
+		<p>
+	
+        Identifiant : <input type="text" name="id" />
+		</br>
+		</br>
+		Mot de passe : <input type="password" name="pas">
+		</br>
+		</br>
+		
+		 <input type="submit" value="Valider" />
+		
+    </body>
+</html>
---
+++ verif.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <?php 
+		
+		
+		
+		if(isset($_POST['id']) and isset($_POST['pas'])){	
+			$id= $_POST['id'];
+			$pas = $_POST['pas'];
+		
+			try
+			{
+				$bdd = new PDO('mysql:host=localhost;dbname=disques;charset=utf8','root','root');
+			}
+				catch (Exception $e)
+			{
+				die('Erreur : ' . $e->getMessage());
+			}
+	
+	
+			$req = $bdd->prepare("Select COUNT(*) as nbre From utilisateurs Where identifiant = ? and mdp = ? ;");
+			$req->execute(array($id,$pas));
+			$donnees = $req->fetch();
+ 
+			if ($donnees['nbre'] == 0) {
+				echo 'identifiant et/ou mot de passe incorrectes';
+			}
+			else {
+				header('Location : menu.html');
+				exit();
+			} 
+		
+			
+
+			$req->closeCursor();
+		
+		
+		}
+		else{
+			echo"veuillez entrer un identifiant et un mot de passe";
+		}
+		
+		?>
+
+    </body>
+</html>
