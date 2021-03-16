227480560aef8eb0d00f3abfe3dced606ce59fac
juliettemnr <59599940+juliettemnr@users.noreply.github.com>
2021-03-16 15:55:42 +0100

    Update ensemble.php

--- ensemble.php
+++ ensemble.php
@@ -4,6 +4,7 @@
 <head>
 
     <meta charset="utf-8" />
+	<link rel="stylesheet" href="style2.css" />
 </head>
 <!-- ATTENTION : POUR ME CONNECTER A LA BASE DE DONNEE J'AI DU CHANGER LE PORT DANS $bdd. JE NE SAIS PAS SI VOUS UTILISEZ LE MÊME PORT, SI NON, LE MODIFIER. -->
 <!-- Désolé des problèmes que ça créé, mais c'était la seule solution que j'ai trouvé pour faire marcher mon MAMP. -->
@@ -13,7 +14,7 @@
 <?php
 try
 {
-    $bdd = new PDO('mysql:host=localhost;port=3307;dbname=disques;charset=utf8', 'root', 'root');
+    $bdd = new PDO('mysql:host=localhost;dbname=disques;charset=utf8','root','root');
 }
 catch(Exception $e)
 {
@@ -25,15 +26,20 @@ $reponse = $bdd->query('SELECT * FROM vinyles');
 while ($donnees = $reponse->fetch())
 {
 ?>
-    <p>
-        <strong>Id :</strong> <?php echo $donnees['id']; ?><br />
-        <strong>Titre :</strong> <?php echo $donnees['titre']; ?><br />
-        <strong>Artiste :</strong> <?php echo $donnees['artiste']; ?><br />
-        <strong>Année de sorte :</strong> <?php echo $donnees['annee_sortie']; ?><br />
-        <strong>Année d'édition :</strong> <?php echo $donnees['annee_edition']; ?><br />
-        <strong>Nombre de disque :</strong> <?php echo $donnees['nb_disque']; ?><br />
-        <strong>Commentaire :</strong> <?php echo $donnees['commentaire']; ?><br />
-    </p>
+    
+        <strong id=vinyle>Vinyle n° <?php echo $donnees['id']; ?><br /> </strong>
+		</br>
+        <strong class =autres>Titre :</strong> <?php echo $donnees['titre']; ?><br />
+        <strong class =autres>Artiste :</strong> <?php echo $donnees['artiste']; ?><br />
+        <strong class =autres>Année de sorte :</strong> <?php echo $donnees['annee_sortie']; ?><br />
+        <strong class =autres>Année d'édition :</strong> <?php echo $donnees['annee_edition']; ?><br />
+        <strong class =autres>Nombre de disque :</strong> <?php echo $donnees['nb_disques']; ?><br />
+        <strong class =autres>Commentaire :</strong> <?php echo $donnees['commentaire']; ?><br />
+    </br>
+	</br>
+	
+	
+	
 <?php
 }
 
